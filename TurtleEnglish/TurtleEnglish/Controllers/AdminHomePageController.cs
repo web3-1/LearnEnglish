a2ad632f3e36c07bf4a71a497f847b6c52a9edd3
devilleo <44858821+devilleo@users.noreply.github.com>
2019-05-22 17:40:11 +0700

    front end Admin Page
    -thiết kế giao diện trang Admin
    -install  gentelella packet

---
+++ TurtleEnglish/TurtleEnglish/Controllers/AdminHomePageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TurtleEnglish.Controllers
+{
+    public class AdminHomePageController : Controller
+    {
+        // GET: Admin_HomePage
+        public ActionResult adminIndex()
+        {
+            return View();
+        }
+
+        public ActionResult userManagement()
+        {
+            return View();
+        }
+
+        public ActionResult vocabularyManagement()
+        {
+            return View();
+        }
+    }
+}
