40a6fd6739e4daf68ec29aea77aba8cca097a98a
devilleo <44858821+devilleo@users.noreply.github.com>
2019-05-31 17:31:01 +0700

    add css, js into Bundles

--- TurtleEnglish/TurtleEnglish/App_Start/BundleConfig.cs
+++ TurtleEnglish/TurtleEnglish/App_Start/BundleConfig.cs
@@ -25,6 +25,34 @@ namespace TurtleEnglish
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            
+            //kha add admin
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                        "~/Style/AdminPageStyle/vendors/bootstrap/dist/css/bootstrap.min.css",
+                        "~/Style/AdminPageStyle/vendors/font-awesome/css/font-awesome.min.css",
+                        "~/Style/AdminPageStyle/vendors/nprogress/nprogress.css",
+                        "~/Style/AdminPageStyle/vendors/bootstrap-daterangepicker/daterangepicker.css",
+                        "~/Style/AdminPageStyle/build/css/custom.min.css"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Style/AdminPageStyle/vendors/jquery/dist/jquery.min.js",
+                        "~/Style/AdminPageStyle/vendors/bootstrap/dist/js/bootstrap.min.js",
+                        "~/Style/AdminPageStyle/vendors/fastclick/lib/fastclick.js",
+                        "~/Style/AdminPageStyle/vendors/nprogress/nprogress.js",
+                        "~/Style/AdminPageStyle/vendors/Chart.js/dist/Chart.min.js",
+                        "~/Style/AdminPageStyle/vendors/jquery-sparkline/dist/jquery.sparkline.min.js",
+                        "~/Style/AdminPageStyle/vendors/Flot/jquery.flot.js",
+                        "~/Style/AdminPageStyle/vendors/Flot/jquery.flot.pie.js",
+                        "~/Style/AdminPageStyle/vendors/Flot/jquery.flot.time.js",
+                        "~/Style/AdminPageStyle/vendors/Flot/jquery.flot.stack.js",
+                        "~/Style/AdminPageStyle/vendors/Flot/jquery.flot.resize.js",
+                        "~/Style/AdminPageStyle/vendors/flot.orderbars/js/jquery.flot.orderBars.js",
+                        "~/Style/AdminPageStyle/vendors/flot-spline/js/jquery.flot.spline.min.js",
+                        "~/Style/AdminPageStyle/vendors/flot.curvedlines/curvedLines.js",
+                        "~/Style/AdminPageStyle/vendors/DateJS/build/date.js",
+                        "~/Style/AdminPageStyle/vendors/moment/min/moment.min.js",
+                        "~/Style/AdminPageStyle/vendors/bootstrap-daterangepicker/daterangepicker.js",
+                        "~/Style/AdminPageStyle/build/js/custom.min.js"
+            ));
         }
     }
 }
